6db7b66a09d1e43245e478245353a6930c844060
Ondrej Babinský <babinskyondrej@gmail.com>
2025-02-20 18:50:02 +0100

    base of the site2

---
+++ src/app/pages/diary/diary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diary',
+  standalone: false,
+  templateUrl: './diary.component.html',
+  styleUrl: './diary.component.css'
+})
+export class DiaryComponent {
+
+}
---
+++ src/app/pages/texts/texts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-texts',
+  standalone: false,
+  templateUrl: './texts.component.html',
+  styleUrl: './texts.component.css'
+})
+export class TextsComponent {
+
+}
